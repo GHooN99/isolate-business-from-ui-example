4396ed160d0b680b339f72df802ca49d4a88be0f
ghoon99 <ghoon99@kakao.com>
2024-10-08 14:20:06 +0900

    feat: 콘솔 숫자야구 게임 구현

--- apps/console-baseball/src/index.ts
+++ apps/console-baseball/src/index.ts
@@ -1,11 +1,45 @@
-import createGameController from '@ibfu/number-baseball';
+import createGameController, { parseResult } from '@ibfu/number-baseball';
+import readline from 'readline';
 
-const controller = createGameController();
+export const getUserInput = (question: string): Promise<string> => {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-controller.start();
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+};
 
-try {
-  console.log(controller.getResult('123'));
-} catch (e) {
-  console.log('debug:', e);
-}
+const main = async () => {
+  const controller = createGameController();
+
+  controller.start();
+  console.log('숫자야구 게임을 시작합니다!');
+  let isRunning = true;
+
+  while (isRunning) {
+    const input = await getUserInput('숫자를 입력하세요: ');
+    const result = controller.getResult(input);
+    console.log(parseResult(result), ',1시도 횟수 :', result.attemptCount);
+
+    if (result.isCorrect) {
+      console.log('정답입니다!');
+      const isContinue = await getUserInput('다시 하시겠습니까? (y/n): ');
+      if (isContinue === 'y') {
+        controller.restart();
+        continue;
+      } else {
+        console.log('게임을 종료합니다.');
+        isRunning = false;
+      }
+    }
+  }
+  process.exit(0);
+};
+
+main();
