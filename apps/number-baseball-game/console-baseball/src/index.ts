a6bbcb6d24ffd7ace33954e3a8a6c7d6eeaf9be6
ghoon99 <ghoon99@kakao.com>
2024-10-08 22:30:11 +0900

    feat(console-baseball): 콘솔 게임 입력값 예외 처리 추가

--- apps/number-baseball-game/console-baseball/src/index.ts
+++ apps/number-baseball-game/console-baseball/src/index.ts
@@ -1,4 +1,9 @@
-import createGameController, { parseResult } from '@ibfu/number-baseball';
+import createGameController, {
+  DuplicateNumberException,
+  InvalidInputLengthException,
+  InvalidInputTypeException,
+  parseResult,
+} from '@ibfu/number-baseball';
 import readline from 'readline';
 
 export const getUserInput = (question: string): Promise<string> => {
@@ -23,19 +28,37 @@ const main = async () => {
   let isRunning = true;
 
   while (isRunning) {
-    const input = await getUserInput('숫자를 입력하세요: ');
-    const result = controller.getResult(input);
-    console.log(parseResult(result), ',1시도 횟수 :', result.attemptCount);
+    try {
+      const input = await getUserInput('숫자를 입력하세요: ');
+      const result = controller.getResult(input);
+      console.log(parseResult(result), ',1시도 횟수 :', result.attemptCount);
 
-    if (result.isCorrect) {
-      console.log('정답입니다!');
-      const isContinue = await getUserInput('다시 하시겠습니까? (y/n): ');
-      if (isContinue === 'y') {
-        controller.restart();
+      if (result.isCorrect) {
+        console.log('정답입니다!');
+        const isContinue = await getUserInput('다시 하시겠습니까? (y/n): ');
+        if (isContinue === 'y') {
+          controller.restart();
+          continue;
+        } else {
+          console.log('게임을 종료합니다.');
+          isRunning = false;
+        }
+      }
+    } catch (e: unknown) {
+      if (e instanceof InvalidInputLengthException) {
+        console.log('입력한 숫자의 길이가 다릅니다. 다시 입력해주세요.');
         continue;
-      } else {
-        console.log('게임을 종료합니다.');
-        isRunning = false;
+      }
+      if (e instanceof DuplicateNumberException) {
+        console.log('중복된 숫자가 있습니다. 다시 입력해주세요.');
+        continue;
+      }
+      if (e instanceof InvalidInputTypeException) {
+        console.log('숫자가 아닌 문자가 포함되어 있습니다. 다시 입력해주세요.');
+        continue;
+      }
+      if (e instanceof Error) {
+        console.log('에러발생!', e.message);
       }
     }
   }
